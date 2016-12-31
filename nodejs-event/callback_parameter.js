6effaffcee5d4eb031ed64a11dd68ef15388455b
Zhongwei Zhou <zhouzhongwei1986@sina.com>
2017-01-01 00:02:28 +0800

    no message

--- nodejs-event/callback_parameter.js
+++ nodejs-event/callback_parameter.js
@@ -1,26 +1,29 @@
 var events = require('events');
+
 function CarShow() {
-	events.EventEmitter.call(this);
-	this.seeCar = function (make) {
-		this.emit('sawCar', make);
-	};
+    events.EventEmitter.call(this);
+    this.seeCar = function(make) {
+        this.emit('sawCar', make);
+    };
 }
 
 CarShow.prototype = new events.EventEmitter();
 
 var show = new CarShow();
+
 function logCar(make) {
-	console.log("Saw a " + make);
+    console.log("Saw a " + make);
 }
+
 function logColorCar(make, color) {
-	console.log("Saw a %s %s", color, make);
+    console.log("Saw a %s %s", color, make);
 }
 
 show.on("sawCar", logCar);
-show.on("sawCar", function (make) {
-	var colors = ['red', 'blue', 'black'];
-	var color = colors[Math.floor(Math.random()*3)];
-	logColorCar(make, color);
+show.on("sawCar", function(make) {
+    var colors = ['red', 'blue', 'black'];
+    var color = colors[Math.floor(Math.random() * 3)];
+    logColorCar(make, color);
 });
 show.seeCar("Ferrari");
 show.seeCar("Porsche");
