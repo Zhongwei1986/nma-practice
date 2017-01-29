2b68e0c04c02568811d6c7010fd8899b9b7ffd83
Zhongwei1986 <zhouzhongwei1986@sina.com>
2017-01-29 21:06:24 +0800

    代码注释

--- 1-NodeJS/nodejs-socket/socket_client.js
+++ 1-NodeJS/nodejs-socket/socket_client.js
@@ -1,16 +1,16 @@
 var net = require('net');
-function getConnection(connName){
-  var client = net.connect({port: 8107, host:'localhost'}, function() {
+function getConnection(connName){    //封装一个函数用来处理net.socket对象的创建和事件监听业务
+  var client = net.connect({port: 8107, host:'localhost'}, function() { //创建一个Socket对象，并指定当连接对服务器打开时的回调函数
     console.log(connName + ' Connected: ');
-    console.log('   local = %s:%s', this.localAddress, this.localPort);
+    console.log('   local = %s:%s', this.localAddress, this.localPort); 
     console.log('   remote = %s:%s', this.remoteAddress, this.remotePort);
-    this.setTimeout(500);
-    this.setEncoding('utf8');
-    this.on('data', function(data) {
-      console.log(connName + " From Server: " + data.toString());
-      this.end();
+    this.setTimeout(500);   //net.Socket对象的方法，和JS标准的setTimeout()不同，只需要传入超时时间delay
+    this.setEncoding('utf8');   //设定默认编码格式
+    this.on('data', function(data) {  //回调函数的参数是Buffer对象
+      console.log(connName + " From Server: " + data.toString());   //Buffer.toString()使用之前设置的默认编码方式
+      this.end();     //主动关闭连接
     });
-    this.on('end', function() {
+    this.on('end', function() { //end事件是服务器发送一个FIN终止连接时发出
       console.log(connName + ' Client disconnected');
     });
     this.on('error', function(err) {
@@ -19,17 +19,17 @@ function getConnection(connName){
     this.on('timeout', function() {
       console.log('Socket Timed Out');
     });
-    this.on('close', function() {
+    this.on('close', function() { //由net.Socket.end()触发
       console.log('Socket Closed');
     });
   });
   return client;
 }
-function writeData(socket, data){
-  var success = !socket.write(data);
-  if (!success){
-    (function(socket, data){
-      socket.once('drain', function(){
+function writeData(socket, data){  //向服务器写数据
+  var success = !socket.write(data);    //调用write()方法向服务器发送数据。如果数据发送完返回true，否则返回false；  
+  if (!success){    //如果success是false，即本地还有内存缓冲区还有数据没有发完
+    (function(socket, data){  //使用闭包保存socket和data
+      socket.once('drain', function(){  //仅监听一次drain事件，当内存缓存清空时，重新发送data
         writeData(socket, data);
       });
     })(socket, data);
--- 1-NodeJS/nodejs-socket/socket_server.js
+++ 1-NodeJS/nodejs-socket/socket_server.js
@@ -1,20 +1,21 @@
 var net = require('net');
-var server = net.createServer(function(client) {
+var server = net.createServer(function(client) { //createServer()创建一个新的net.Server对象,其回调函数默认作为connection事件的的处理程序
+  // 当服务器收到一个连接时，会创建一个Socket对象，传给上述连接事件处理程序
   console.log('Client connection: ');
   console.log('   local = %s:%s', client.localAddress, client.localPort);
   console.log('   remote = %s:%s', client.remoteAddress, client.remotePort);
-  client.setTimeout(500);
+  client.setTimeout(500);  
   client.setEncoding('utf8');
-  client.on('data', function(data) {
-    console.log('Received data from client on port %d: %s', 
+  client.on('data', function(data) {     //监听客户端发来的数据事件
+    console.log('Received data from client on port %d: %s',
                 client.remotePort, data.toString());
-    console.log('  Bytes received: ' + client.bytesRead);
+    console.log('  Bytes received: ' + client.bytesRead); // 读取的字节数
     writeData(client, 'Sending: ' + data.toString());
-    console.log('  Bytes sent: ' + client.bytesWritten);
+    console.log('  Bytes sent: ' + client.bytesWritten);  //写入的字节数
   });
   client.on('end', function() {
     console.log('Client disconnected');
-    server.getConnections(function(err, count){
+    server.getConnections(function(err, count){  //返回当前连接到服务器的连接数量
       console.log('Remaining Connections: ' + count);
     });
   });
@@ -25,9 +26,9 @@ var server = net.createServer(function(client) {
     console.log('Socket Timed out');
   });
 });
-server.listen(8107, function() {
-  console.log('Server listening: ' + JSON.stringify(server.address()));
-  server.on('close', function(){
+server.listen(8107, function() {   //打开8107端口并开始监听连接。当端口打开时，回调函数调用
+  console.log('Server listening: ' + JSON.stringify(server.address()));  //返回绑定的地址
+  server.on('close', function(){  
     console.log('Server Terminated');
   });
   server.on('error', function(err){
